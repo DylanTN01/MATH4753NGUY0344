1d59e59e343c2f4c8affc0ebfc5e184f05bbf327
DylanTN01 <DylanTN.01@gmail.com>
2022-05-02 21:21:54 -0500

    Insert data

---
+++ R/ddt.R
@@ -0,0 +1,17 @@
+#' DDT Data Set for Fish
+#'
+#' 144 lines ofmeasurements of fish
+#'
+#'
+#' @format A data frame with 144 rows and 6 columns
+#' \describe{
+#'   \item{RIVER}{Location of fish}
+#'   \item{MILE}{Location of the fish within the river}
+#'   \item{SPECIES}{Species of fish}
+#'   \item{LENGTH}{Length of fish}
+#'   \item{WEIGHT}{Weight of fish}
+#'   \item{DDT}{DDT level}
+#'   ...
+#' }
+#' @source {Textbook}
+"ddt"
